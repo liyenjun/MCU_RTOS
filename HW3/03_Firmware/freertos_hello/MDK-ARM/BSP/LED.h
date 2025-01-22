116b06b748fcb5d771bd15d650b01cd7a32f509d
YCL <liyenjun@gmail.com>
2025-01-22 20:05:18 +0800

    HW3_20250122_v1

---
+++ HW3/03_Firmware/freertos_hello/MDK-ARM/BSP/LED.h
@@ -0,0 +1,41 @@
+/******************************************************************************
+ * Copyright (C) 2025 xxx, Inc.(Gmbh) or its affiliates.
+ * 
+ * All Rights Reserved.
+ * 
+ * @file LED.h
+ * 
+ * @par dependencies 
+ * - ec_bsp_aht21_reg.h
+ * - stdio.h
+ * - stdint.h
+ * 
+ * @author YC | R&D Dept. | xxx, Inc.
+ * 
+ * @brief Provide the HAL APIs of AHT21 and corresponding opetions.
+ * 
+ * Processing flow:
+ * 
+ * call directly.
+ * 
+ * @version V1.0 2025-01-22
+ *
+ * @note 1 tab == 4 spaces!
+ * 
+ *****************************************************************************/
+#ifndef __LED_H__  //Avoid repeated including same files later
+#define __LED_H__
+
+//******************************** Includes *********************************//
+
+//******************************** Includes *********************************//
+
+//******************************** Defines **********************************//
+
+//******************************** Defines **********************************//
+
+//******************************** Declaring ********************************//
+                                                                                                
+//******************************** Declaring ********************************//
+												
+#endif
